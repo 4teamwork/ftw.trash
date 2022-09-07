f74751363fbf983d3445a504f0691693655ad861
BHUVAN-RJ <83565908+BHUVAN-RJ@users.noreply.github.com>
2022-09-07 18:55:09 +0530

    Update test_plone.py

--- ftw/trash/tests/test_plone.py
+++ ftw/trash/tests/test_plone.py
@@ -1,72 +1,176 @@
-# from ftw.builder import Builder, create
-# from ftw.trash.tests import FunctionalTestCase, duplicate_with_dexterity
-# from ftw.trash.trasher import Trasher
+from ftw.trash.testing import FTW_TRASH_INTEGRATION_TESTING
+from ftw.trash.trasher import Trasher
+from ftw.trash.interfaces import (
+    IRestorable,
+    ITrashed,
+)
+
+from plone.app.testing import TEST_USER_ID, setRoles
+from plone import api
+
+import unittest
 
 
-# @duplicate_with_dexterity
-# class TestPloneIntegration(FunctionalTestCase):
-#     def test_folder_objectItems_returns_trashed_content(self):
-#         """A folder's objectItems should return trashed content.
-#         """
-#         self.grant("Contributor")
-#         container = create(Builder("folder").titled(u"Container"))
-#         foo = create(Builder("folder").titled(u"Foo").within(container))
-#         bar = create(Builder("folder").titled(u"Bar").within(container))
-#         self.assertEqual([("foo", foo), ("bar", bar)], list(container.objectItems()))
-#         Trasher(foo).trash()
-#         self.assertEqual([("foo", foo), ("bar", bar)], list(container.objectItems()))
+class TestPloneIntegration(unittest.TestCase):
+    layer = FTW_TRASH_INTEGRATION_TESTING
 
-#     def test_plone_objectItems_returns_trashed_content(self):
-#         """A Plone site's objectItems should return trashed content.
-#         """
-#         self.grant("Contributor")
-#         foo = create(Builder("folder").titled(u"Foo"))
-#         self.assertIn(("foo", foo), list(self.portal.objectItems()))
-#         Trasher(foo).trash()
-#         self.assertIn(("foo", foo), list(self.portal.objectItems()))
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer["portal"]
 
-#     def test_folder_contentItems_does_not_return_trashed_content(self):
-#         """A folder's contentItems should not return trashed content.
-#         """
-#         self.grant("Contributor")
-#         container = create(Builder("folder").titled(u"Container"))
-#         foo = create(Builder("folder").titled(u"Foo").within(container))
-#         bar = create(Builder("folder").titled(u"Bar").within(container))
-#         self.assertEqual([("foo", foo), ("bar", bar)], container.contentItems())
-#         Trasher(foo).trash()
-#         self.assertEqual([("bar", bar)], container.contentItems())
+    def assert_provides(self, obj, *expected):
+        expected = set([_f for _f in expected if _f])
+        got = {iface for iface in (IRestorable, ITrashed) if iface.providedBy(obj)}
+        self.assertEquals(
+            expected, got, "Unexpected interfaces provided by {!r}".format(obj)
+        )
 
-#     def test_plone_contentItems_does_not_return_trashed_content(self):
-#         """A Plone site's contentItems should not return trashed content.
-#         """
-#         self.grant("Contributor")
-#         foo = create(Builder("folder").titled(u"Foo"))
-#         self.assertIn(("foo", foo), self.portal.contentItems())
-#         Trasher(foo).trash()
-#         self.assertNotIn(("foo", foo), self.portal.contentItems())
 
-#     def test_listFolderContents_does_not_return_trashed_content(self):
-#         """A folder's listFolderContents should not return trashed content.
-#         """
-#         self.grant("Contributor")
-#         container = create(Builder("folder").titled(u"Container"))
-#         foo = create(Builder("folder").titled(u"Foo").within(container))
-#         bar = create(Builder("folder").titled(u"Bar").within(container))
-#         self.assertEqual([foo, bar], container.listFolderContents())
-#         Trasher(foo).trash()
-#         self.assertEqual([bar], container.listFolderContents())
+    def test_folder_objectItems_returns_trashed_content(self):
+        """A folder's objectItems should return trashed content.
+        """
+        setRoles(self.portal, TEST_USER_ID, ["Contributor"])
 
-#     def test_getFolderContents_does_not_return_trashed_content(self):
-#         """A folder's getFolderContents should not return trashed content.
-#         """
-#         self.grant("Contributor")
-#         container = create(Builder("folder").titled(u"Container"))
-#         foo = create(Builder("folder").titled(u"Foo").within(container))
-#         bar = create(Builder("folder").titled(u"Bar").within(container))
-#         self.assertEqual(
-#             [foo, bar], [brain.getObject() for brain in container.getFolderContents()]
-#         )
-#         Trasher(foo).trash()
-#         self.assertEqual(
-#             [bar], [brain.getObject() for brain in container.getFolderContents()]
-#         )
+        container = api.content.create(
+            container=self.portal,
+            type="Folder",
+            id="parent",
+            title="Parent",
+        )
+        
+        foo = api.content.create(
+            container=container,
+            type="Folder",
+            id="foo",
+            title="Foo",
+        )
+        bar = api.content.create(
+            container=container,
+            type="Folder",
+            id="bar",
+            title="Bar",
+        )
+        self.assertEqual([("foo", foo), ("bar", bar)], list(container.objectItems()))
+        
+        Trasher(foo).trash()
+        self.assertEqual([("foo", foo), ("bar", bar)], list(container.objectItems()))
+
+    def test_plone_objectItems_returns_trashed_content(self):
+        """A Plone site's objectItems should return trashed content.
+        """
+        setRoles(self.portal, TEST_USER_ID, ["Contributor"])
+
+        foo = api.content.create(
+            container=self.portal,
+            type="Folder",
+            id="foo",
+            title="Foo",
+        )
+        self.assertIn(("foo", foo), list(self.portal.objectItems()))
+        
+        Trasher(foo).trash()
+        self.assertIn(("foo", foo), list(self.portal.objectItems()))
+
+    def test_folder_contentItems_does_not_return_trashed_content(self):
+        """A folder's contentItems should not return trashed content.
+        """
+        setRoles(self.portal, TEST_USER_ID, ["Contributor"])
+
+        container = api.content.create(
+            container=self.portal,
+            type="Folder",
+            id="parent",
+            title="Parent",
+        )
+        foo = api.content.create(
+            container=container,
+            type="Folder",
+            id="foo",
+            title="Foo",
+        )
+        bar = api.content.create(
+            container=container,
+            type="Folder",
+            id="bar",
+            title="Bar",
+        )
+        self.assertEqual([("foo", foo), ("bar", bar)], container.contentItems())
+        
+        Trasher(foo).trash()
+        self.assertEqual([("bar", bar)], container.contentItems())
+
+    def test_plone_contentItems_does_not_return_trashed_content(self):
+        """A Plone site's contentItems should not return trashed content.
+        """
+        setRoles(self.portal, TEST_USER_ID, ["Contributor"])
+        
+        foo = api.content.create(
+            container=self.portal,
+            type="Folder",
+            id="foo",
+            title="Foo",
+        )
+        self.assertIn(("foo", foo), self.portal.contentItems())
+        
+        Trasher(foo).trash()
+        self.assertNotIn(("foo", foo), self.portal.contentItems())
+
+    def test_listFolderContents_does_not_return_trashed_content(self):
+        """A folder's listFolderContents should not return trashed content.
+        """
+        setRoles(self.portal, TEST_USER_ID, ["Contributor"])
+
+        container = api.content.create(
+            container=self.portal,
+            type="Folder",
+            id="parent",
+            title="Parent",
+        )
+        foo = api.content.create(
+            container=container,
+            type="Folder",
+            id="foo",
+            title="Foo",
+        )
+        bar = api.content.create(
+            container=container,
+            type="Folder",
+            id="bar",
+            title="Bar",
+        )
+        self.assertEqual([foo, bar], container.listFolderContents())
+        
+        Trasher(foo).trash()
+        self.assertEqual([bar], container.listFolderContents())
+
+    def test_getFolderContents_does_not_return_trashed_content(self):
+        """A folder's getFolderContents should not return trashed content.
+        """
+        setRoles(self.portal, TEST_USER_ID, ["Contributor"])
+
+        container = api.content.create(
+            container=self.portal,
+            type="Folder",
+            id="parent",
+            title="Parent",
+        )
+        foo = api.content.create(
+            container=container,
+            type="Folder",
+            id="foo",
+            title="Foo",
+        )
+        bar = api.content.create(
+            container=container,
+            type="Folder",
+            id="bar",
+            title="Bar",
+        )
+        self.assertEqual(
+            [foo, bar], [brain.getObject() for brain in container.getFolderContents()]
+        )
+        
+        Trasher(foo).trash()
+        self.assertEqual(
+            [bar], [brain.getObject() for brain in container.getFolderContents()]
+        )
